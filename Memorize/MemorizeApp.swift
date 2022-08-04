1482fd15c70c38854e4cca3b1b84fa88b07e6171
Trevor Davis <trevormckay.davis@gmail.com>
2022-08-04 08:31:37 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Trevor Davis on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
